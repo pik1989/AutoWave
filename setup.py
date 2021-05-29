153613c11eb202a92b7b550dda1cee94c11c6029
= <=>
2021-05-30 00:13:49 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages
+
+setup(name="PyAutoSpeech",
+version="0.1",
+description="Package contains functionality for speech analysis",
+Long_description="",
+packages=['PyAutoSpeech'],
+install_requires=["pydub",]
+)
