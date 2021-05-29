00bb1d35a34669c24cdd44fe35957f188d90eb5e
= <=>
2021-05-30 00:13:09 +0530

    Create pitch_shifter.py

---
+++ build/lib/PyAutoSpeech/pitch_shifter.py
@@ -0,0 +1,17 @@
+from pydub import AudioSegment
+def pitchShifter(audioname,shift,value):
+    formats_to_convert = ['.wav']  
+    if audioname.endswith(tuple(formats_to_convert)):
+    
+        audio=AudioSegment.from_wav(audioname)
+        if shift=='i':
+
+            # Increase the volume by 10 dB 
+            return(audio + value)
+        elif shift=='d':
+            # Reducing volume by 5
+            return(audio1 - value)
+        else:
+            print("Error in shifting the pitch")
+    else:
+        print('Please use .wav file')
