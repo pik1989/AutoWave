fcc6191b53b401bf7df799f50f1a8ca11737b729
= <37014842+erickeagle@users.noreply.github.com>
2021-05-31 23:07:50 +0530

    Update __init__.py

--- PyAutoSpeech/__init__.py
+++ PyAutoSpeech/__init__.py
@@ -2,3 +2,4 @@ import os
 from .speech_conversion import speechConversion
 from .pitch_shifter import pitchShifter
 from .audio_player import AudioPlayer
+from .augumentor import augumentOneFile,augumentFolderRate, augumentFolderItem
