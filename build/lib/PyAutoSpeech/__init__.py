54dd596e420974abce5f443bf12f6d37d222e2e6
= <37014842+erickeagle@users.noreply.github.com>
2021-06-02 17:09:23 +0530

    Delete __init__.py

--- build/lib/PyAutoSpeech/__init__.py
+++
@@ -1,5 +0,0 @@
-import os
-from .speech_conversion import speechConversion
-from .pitch_shifter import pitchShifter
-from .audio_player import AudioPlayer
-from .augumentor import augumentOneFile,augumentFolderRate, augumentFolderItem
