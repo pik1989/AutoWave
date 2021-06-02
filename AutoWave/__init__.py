e12d026eee0ee47166e82c21a00dc4f937e80585
= <37014842+erickeagle@users.noreply.github.com>
2021-06-02 17:08:34 +0530

    Create __init__.py

---
+++ AutoWave/__init__.py
@@ -0,0 +1,5 @@
+import os
+from .speech_conversion import speechConversion
+from .pitch_shifter import pitchShifter
+from .audio_player import AudioPlayer
+from .augumentor import augumentOneFile,augumentFolderRate, augumentFolderItem
