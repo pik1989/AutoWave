0a1b49e019112a3531af555ef806f6281bc6c5c6
= <37014842+erickeagle@users.noreply.github.com>
2021-06-05 19:27:55 +0530

    Update __init__.py

--- AutoWave/__init__.py
+++ AutoWave/__init__.py
@@ -3,3 +3,4 @@ from .speech_conversion import speechConversion
 from .pitch_shifter import pitchShifter
 from .audio_player import AudioPlayer
 from .augumentor import augumentOneFile,augumentFolderRate, augumentFolderItem
+from .plotting import plotOneFile, plotMultipleFile
