0fcba9fc143a0a20707dda2c0728b0978e772feb
= <37014842+erickeagle@users.noreply.github.com>
2021-06-14 15:06:04 +0530

    Update pitch_shifter.py

--- build/lib/AutoWave/pitch_shifter.py
+++ build/lib/AutoWave/pitch_shifter.py
@@ -1,15 +1,21 @@
 from pydub import AudioSegment
 def pitchShifter(audioname,shift,value):
+    '''
+    this functions takes the wav file and returns the pitch shifted file, i.e, increase and decrease the volume of the audio file
+    audioname:- name of the audio file/path to the audio file
+    shift:- 'i' for increasing the volume/ 'd' for decreasing the volume
+    value:- unit by which increase and decrease the volume
+    '''
     formats_to_convert = ['.wav']  
     if audioname.endswith(tuple(formats_to_convert)):
     
         audio=AudioSegment.from_wav(audioname)
         if shift=='i':
 
-            # Increase the volume by 10 dB 
+            # Increase the volume  
             return(audio + value)
         elif shift=='d':
-            # Reducing volume by 5
+            # Reducing volume 
             return(audio - value)
         else:
             print("Error in shifting the pitch")
